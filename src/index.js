39276abe857faea29dbefb965ab93199edd4f137
jaisingh051297 <>
2024-04-13 00:07:53 +0530

    Add ApiError and ApiResponse class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db.js";
+import {app} from "./app.js"
 
 dotenv.config({
-    path:"./env"
+    path:"./.env"
 })
 
+const port = process.env.PORT || 8000;
 
+// connecting Database(MongoDB Atlas).
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(`Error : ${error}`)
+        throw error;
+    })
+
+    app.listen(port,()=>{
+        console.log(`⌚  Server is running at Port ${port}`)
+    })
+
+})
+.catch((error)=>{
+    console.log(`Mongo DB connection failed !!! ${error}`)
+
+})
